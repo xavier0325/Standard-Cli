48cb3af0ccfe7af43b204cbf7d601c092e7cea57
xavier <xavier0325@outlook.com>
2022-08-29 17:21:27 +0800

    🌟feat(all): 初始化

---
+++ src/views/home.vue
@@ -0,0 +1,17 @@
+<!--
+ * new page
+ * @author: xuyifeng
+ * @since: 2022-08-26
+ * home.vue
+-->
+<template>
+  <div class="container"></div>
+</template>
+<script lang="ts">
+export default {
+  name: 'MyHome'
+}
+</script>
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  base: './', // 设置打包路径
+  server: {
+    port: 4000,
+    open: true,
+    cors: true
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
+  }
+})
